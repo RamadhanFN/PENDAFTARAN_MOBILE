9caa4fdd6d1df95b5b2dc098c57ccb22610cf560
RamadhanFN <ramadhanfirda27@gmail.com>
2020-03-30 13:35:14 +0700

    Design Commit

--- app/src/main/java/com/example/pendaftaran_mobile/MainActivity.java
+++ app/src/main/java/com/example/pendaftaran_mobile/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.pendaftaran_mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dataoptionmenu, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.about){
+            startActivity(new Intent(this, About.class));
+        } else if (item.getItemId() == R.id.setting) {
+            startActivity(new Intent(this, Setting.class));
+        } else if (item.getItemId() == R.id.help) {
+            startActivity(new Intent(this, Help.class));
+        }
+
+        return true;
     }
 }
